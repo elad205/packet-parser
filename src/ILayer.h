70ae6f34f70c6d94b4348cbb9df3da508ef8a4de
elad <eladcyn@gmail.com>
2022-06-20 23:37:45 +0300

    Deserialize concpet is now with const buffer

--- src/ILayer.h
+++ src/ILayer.h
@@ -10,7 +10,7 @@ namespace Layers
 
 /* Each layer have to implement a way to make raw data into an object */
 template<typename Layer>
-concept Deserializable = requires(Layer &l, uint8_t *data)
+concept Deserializable = requires(Layer &l, const uint8_t *data)
 {
     { l.Deserialize(data) } -> std::same_as<bool>;
 };
